2a9866786ec1d0a10deffc5f93b30d6f6cafb153
Sergey Litvinov <slitvinov@gmail.com>
2024-03-16 17:59:57 -0400

    add wait

--- cgi-bin/pydoc.c
+++ cgi-bin/pydoc.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 static const char name[] = "pydoc=";
 int main() {
   char *query_string;
+  int status;
   if (puts("Content-Type: text/plain\n") < 0)
     return 1;
   query_string = getenv("QUERY_STRING");
   if (query_string == NULL || strncmp(name, query_string, sizeof name - 1) != 0)
     return 1;
   query_string += sizeof name - 1;
-  execlp("python", "python", "-m", "pydoc", query_string, (char*)NULL);
+  if (fork() == 0)
+    execlp("python", "python", "-m", "pydoc", query_string, (char*)NULL);
+  wait(&status);
 }
