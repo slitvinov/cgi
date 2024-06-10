09de8971f2bf4faa6ad89fc3875dc0f0bd7f2823
Sergey Litvinov <slitvinov@gmail.com>
2024-06-10 12:39:40 +0200

    up

--- cgi-bin/pydoc.c
+++ cgi-bin/pydoc.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/wait.h>
 #include <unistd.h>
 static char *url_decode(char *);
 static const char name[] = "pydoc=";
 int main() {
   char *query_string, *query_decoded;
-  int status;
   if (puts("Content-Type: text/plain\n") < 0)
     return 1;
   query_string = getenv("QUERY_STRING");
@@ -20,7 +20,7 @@ int main() {
     execlp("python", "python", "-m", "pydoc", query_decoded, (char *)NULL);
 }
 
-static car from_hex(car c) {
+static char from_hex(char c) {
   return isdigit(c) ? c - '0' : tolower(c) - 'a' + 10;
 }
 
@@ -29,8 +29,8 @@ static char *url_decode(char *s) {
   while (*s) {
     if (*s == '%') {
       if (s[1] && s[2]) {
-        *p++ = from_hex(s[1]) << 4 | from_hex(s[2]);
-        s += 2;
+	*p++ = from_hex(s[1]) << 4 | from_hex(s[2]);
+	s += 2;
       }
     } else if (*s == '+') {
       *p++ = ' ';
