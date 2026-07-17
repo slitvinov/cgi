d60bafbb807c5d9810800bd7143819a9825363f3
Sergey Litvinov <slitvinov@gmial.com>
2026-07-18 00:52:48 +0200

    add escape

--- cgi-bin/c.c
+++ cgi-bin/c.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void html_escape(const char *s) {
+  for (; *s; s++) {
+    switch (*s) {
+    case '<':
+      fputs("&lt;", stdout);
+      break;
+    case '>':
+      fputs("&gt;", stdout);
+      break;
+    case '&':
+      fputs("&amp;", stdout);
+      break;
+    case '"':
+      fputs("&quot;", stdout);
+      break;
+    case '\'':
+      fputs("&#39;", stdout);
+      break;
+    default:
+      putchar(*s);
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   const char *value;
   puts("\
@@ -10,16 +34,19 @@ Content-type: text/html\n\
   <HEAD><TITLE>c</TITLE></HEAD>\n\
   <BODY>\n\
     <PRE>");
-  while (*++argv)
-    printf("%s\n", *argv);
+  while (*++argv) {
+    html_escape(*argv);
+    putchar('\n');
+  }
   value = getenv("PATH_INFO");
   if (value) {
     fputs("PATH_INFO='", stdout);
-    fputs(value, stdout);
+    html_escape(value);
     putchar('\'');
   }
   puts("\
     </PRE>\n\
   </BODY>\n\
 </HTML>");
+  return 0;
 }
